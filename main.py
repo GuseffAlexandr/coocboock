1a70c25f345b594045522dca24ee19232caae12f
Alexandr Gusev <aleksari07@gmail.com>
2024-07-20 21:36:11 +0300

    Task 2

--- main.py
+++ main.py
@@ -25,8 +25,28 @@ def read_cooc_book(filename: str):
     return cooc_book
 
 
+def get_shop_list_by_dishes(cooc_book, dishes, person_count):
+    count_food = {}
+    for dish in dishes:
+        recipie = cooc_book[dish]
+        for item in recipie:
+            name = item['ingredient_name']
+            count = item['quantity']
+            measure = item['measure']
+            if name in count_food:
+                count_food[name]['quantity'] += person_count * count
+            else:
+                count_food[name] = {
+                    'quantity': person_count * count,
+                    'measure': measure,
+                }
+    return count_food
+
+
 if __name__ == '__main__':
     cooc_book = read_cooc_book('recipes.txt')
     print(cooc_book)
+
+    print(get_shop_list_by_dishes(cooc_book, ['Омлет', 'Омлет', 'Запеченный картофель'], 3))
 
 
