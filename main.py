dd1fba5d629e9ec28703bc6817c43833eae066fd
Alexandr Gusev <aleksari07@gmail.com>
2024-07-20 21:21:12 +0300

    Task 1

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+def read_ingridient(item: str):
+    items = item.split(' | ')
+    return {
+        'ingredient_name': items[0],
+        'quantity': int(items[1]),
+        'measure': items[2]
+    }
+
+
+def read_cooc_book(filename: str):
+    cooc_book = {}
+    with open(filename, 'r') as fout:
+        while True:
+            name = fout.readline()
+            if name == '':
+                break
+            count = int(fout.readline())
+            cooc_book[name] = []
+            for _ in range(count):
+                ingr = fout.readline()
+                cooc_book[name].append(read_ingridient(ingr))
+            fout.readline()
+    return cooc_book
+
+
+if __name__ == '__main__':
+    cooc_book = read_cooc_book('recipes.txt')
+    print(cooc_book)
+
+
